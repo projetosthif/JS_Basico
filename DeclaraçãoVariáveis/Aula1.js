ae1e93e49fe268d049828e41d82f21625913ad63
Thifany Garcia <100360093+projetosthif@users.noreply.github.com>
2023-08-01 11:43:03 -0300

    Add files via upload

---
+++ DeclaraçãoVariáveis/Aula1.js
@@ -0,0 +1,12 @@
+// o js aceita as duas formas tanto escrevendo var ou n
+
+/* 
+var nome = 'Thifany'
+idade = '16 anos'
+var saldo = '0,15'
+ */
+
+nome = prompt("Qual é o seu nome?")
+idade = parseInt(prompt("Qual é a sua idade?"))
+curso = prompt("Qual é o seu curso?")
+
